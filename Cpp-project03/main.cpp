631d11a7723c675edb0b68c90e19800694707c74
Leonardo <leonardod16b@gmail.com>
2026-07-16 05:48:54 -0500

    Add files via upload

---
+++ Cpp-project03/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int SecondProgram();
+
+int main() {
+    std::cout << "The first program" << std::endl;
+    SecondProgram();
+    return 0;
+}   
---
+++ Cpp-project03/main02.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int SecondProgram() {
+    cout << "The second program" << endl;
+    return 0;
+}
