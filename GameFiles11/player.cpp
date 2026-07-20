f8bb84d099e1da97aa1dc42c7834c60f70331f79
Leonardo <leonardod16b@gmail.com>
2026-07-19 20:23:59 -0500

    Delete GameFiles11 directory

--- GameFiles11/player.cpp
+++
@@ -1,8 +0,0 @@
-#include "player.hpp"
-
-void MovePlayer(Player &player) {
-    if (IsKeyDown(KEY_W)) player.position.z -= player.speed;
-    if (IsKeyDown(KEY_S)) player.position.z += player.speed;
-    if (IsKeyDown(KEY_A)) player.position.x -= player.speed;
-    if (IsKeyDown(KEY_D)) player.position.x += player.speed;
-}
