0a841b2ac16741fa5c1fce62152e027816192fa4
Leonardo <leonardod16b@gmail.com>
2026-08-15 20:41:49 -0500

    Add files via upload

---
+++ Cpp-project01/include/example.hpp
@@ -0,0 +1,7 @@
+// example.hpp
+#ifndef example_hpp // lowercase macros
+#define example_hpp 
+
+void change(int& number); // function declaration
+
+#endif // example.hpp
---
+++ Cpp-project01/src/example.cpp
@@ -0,0 +1,8 @@
+// example.cpp
+#include <iostream>
+#include "example.hpp"
+
+void change(int& number)
+{
+    number = 10;
+}
